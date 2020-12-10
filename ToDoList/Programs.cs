dd5906875eb0709dd72d24d2face2b5aedb2632e
Constantine <faustlarsen@gmail.com>
2020-12-09 19:59:53 -0700

    add an item

--- ToDoList/Programs.cs
+++ ToDoList/Programs.cs
@@ -2,7 +2,6 @@ using System;
 using ToDoList.Models;
 using System.Collections.Generic;
 
-
 namespace ToDoList
 {
   public class Program
@@ -10,41 +9,37 @@ namespace ToDoList
     public static void Main()
     {
       Console.WriteLine("Welcome to the To Do List.");
-      Console.WriteLine("Would you like to add an item to your list? ['Y' for yes, 'Enter' for no]");
+      Console.WriteLine("Would you like to add or view an item to your list? Type add or view");
       string userInput = Console.ReadLine();
-      if (userInput == "Y" || userInput == "y")
+      if (userInput == "add")
       {
-        AddNewItems();
+        AddNewItem();
       }
-      else
-      {
-        Console.WriteLine("Would you like to view your list of items? ['Y' for yes, 'Enter' for no]")
-        string viewItems = Console.ReadLine();
-        if (finishedAnswer == "Y" || finishedAnswer == "y")
+        else if (userInput == "view")
       {
         ListAllItems();
       }
-      else
-      {
-        Main();
-      }
-    }  
-    
-      public static void AddNewItems();
-      {
-        Console.WriteLine("What would you like to add?"); 
-        string itemAdded = Console.ReadLine();
-        Item newItem = new Item(itemAdded);
-        ListAllItems();
-      }
-          
-
-      public static void ListAllItems()
-    {
-      List<Item> allThings = Item.GetAll();
+    }     
       
+    public static void AddNewItem()
+    {
+      Console.WriteLine("What would you like to add?"); 
+      string itemAdded = Console.ReadLine();
+      Item newItem = new Item(itemAdded);
+      ListAllItems();
     }
-    
+          
+    public static void ListAllItems()
+    {
+      List<Item> fullList = Item.GetAll();
+      if (fullList.Count > 0)
+      {
+        Console.WriteLine("TO-DO list:");
+        foreach (Item addedItem in fullList)
+        {
+          Console.WriteLine(addedItem.Description);
+        }
+      }
+    }
   }
-} 
-
+}
